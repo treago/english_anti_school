46279f964cf910425c454e0ce58d513e7922516e
Vladyslav Bondarenko <vlad@medentee.com>
2024-07-17 10:02:04 +0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,96 @@
+import Swiper from "swiper/bundle";
+import { Navigation, Autoplay, EffectFade } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/bundle";
+
+// Main swiper
+const swiper = new Swiper(".about .swiper", {
+  modules: [Navigation, Autoplay, EffectFade],
+  slidesPerView: 1,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  effect: "fade",
+  navigation: {
+    nextEl: ".slider-btn--next",
+    prevEl: ".slider-btn--prev",
+  },
+});
+
+// Decrease header height on scroll
+window.addEventListener("scroll", () => {
+  const headerElement = document.getElementById("header");
+
+  if (window.scrollY > headerElement.offsetHeight) {
+    headerElement.classList.add("scrolled");
+  } else {
+    headerElement.classList.remove("scrolled");
+  }
+});
+
+const handleCloseNav = () => {
+  const navElement = document.getElementById("nav");
+  const burgerElement = document.getElementById("burger");
+
+  navElement.classList.remove("active");
+  burgerElement.classList.remove("active");
+};
+
+// Open menu on burger click
+const handleBurgerClick = () => {
+  const navElement = document.getElementById("nav");
+  const burgerElement = document.getElementById("burger");
+
+  navElement.classList.toggle("active");
+  burgerElement.classList.toggle("active");
+};
+
+// Header menu navigation
+const handleNavItemClick = (anchorId) => {
+  const anchorElement = document.getElementById(anchorId);
+  const header = document.getElementById("header");
+  const headerHeight = header.clientHeight;
+  const top = anchorElement.offsetTop - headerHeight;
+
+  window.scrollTo({ behavior: "smooth", top });
+  handleCloseNav()
+};
+
+document.addEventListener("click", (e) => {
+  if (e.target.id === "burger") {
+    handleBurgerClick();
+  }
+
+  const navLinkElement = e.target.closest(".nav__link");
+
+  if (navLinkElement) {
+    e.preventDefault();
+
+    handleNavItemClick(navLinkElement.hash.slice(1));
+  }
+});
+
+// Handle discount modal
+const modal = document.getElementById("discountModal");
+const modalButton = document.getElementById("discountModalButton");
+const span = document.querySelector("#discountModal .close");
+
+setTimeout(() => {
+  modal.style.display = "block";
+}, 30 * 1000);
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+modalButton.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
